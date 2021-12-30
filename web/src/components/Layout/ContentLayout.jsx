0b7e05c8032dad1f18986cb6d4fc685eee63d6d3
joonaramo <joona00ramo@gmail.com>
2021-12-30 11:25:42 +0200

    feat(frontend): support dark background on content layout

--- web/src/components/Layout/ContentLayout.jsx
+++ web/src/components/Layout/ContentLayout.jsx
@@ -1,8 +1,11 @@
-export const ContentLayout = ({ title, children }) => {
+import { classNames } from '../../utils/classnames';
+export const ContentLayout = ({ title, children, darkBg = false }) => {
   return (
-    <div className='py-6'>
+    <div
+      className={classNames(darkBg ? 'text-white' : 'text-gray-900', 'py-6')}
+    >
       <div className='max-w-7xl mx-auto px-4 sm:px-6 md:px-8'>
-        <h1 className='text-2xl font-semibold text-gray-900'>{title}</h1>
+        <h1 className='text-2xl font-semibold'>{title}</h1>
       </div>
       <div className='max-w-7xl mx-auto px-4 sm:px-6 md:px-8'>{children}</div>
     </div>
