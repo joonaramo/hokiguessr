32f348a4619589b379e5acc5a5a1b4e2ef5eb894
joonaramo <joona00ramo@gmail.com>
2022-01-02 17:10:16 +0200

    chore(frontend): improving react-query queries and disabling refetch on window focus

--- web/src/App.js
+++ web/src/App.js
@@ -2,12 +2,20 @@ import { AppRoutes } from './routes';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { AuthProvider } from './lib/auth';
 import { ReactQueryDevtools } from 'react-query/devtools';
+import { Notifications } from './components/Notifications';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
+      <Notifications />
       <AuthProvider>
         <AppRoutes />
       </AuthProvider>
--- web/src/components/LiveGames/index.jsx
+++ web/src/components/LiveGames/index.jsx
@@ -3,16 +3,58 @@ import { useQuery } from 'react-query';
 import liigaService from '../../services/liiga';
 import { GameCard } from './GameCard';
 import { Spinner } from '../Spinner';
+import { RefreshIcon } from '@heroicons/react/outline';
+import { classNames } from '../../utils/classnames';
+import { useNotificationStore } from '../../stores/notification';
 
 export const LiveGames = () => {
-  const games = useQuery('liveGames', liigaService.getLiveGames);
+  const { addNotification } = useNotificationStore();
+  const games = useQuery('liveGames', liigaService.getLiveGames, {
+    onSuccess: (data) => {
+      const prevData = games.data;
+      if (prevData) {
+        let goals = [];
+        data.forEach((game) => {
+          const homeGoalEvents = game.homeTeam.goalEvents;
+          goals = goals.concat(homeGoalEvents);
+          const awayGoalEvents = game.awayTeam.goalEvents;
+          goals = goals.concat(awayGoalEvents);
+        });
+        let oldGoals = [];
+        prevData.forEach((game) => {
+          const homeGoalEvents = game.homeTeam.goalEvents;
+          oldGoals = oldGoals.concat(homeGoalEvents);
+          const awayGoalEvents = game.awayTeam.goalEvents;
+          oldGoals = oldGoals.concat(awayGoalEvents);
+        });
+        const newGoals = goals.filter(
+          (goal) =>
+            !JSON.stringify(oldGoals).includes(`"eventId":${goal.eventId}`) &&
+            !goal.goalTypes.includes('VL')
+        );
+        newGoals.forEach((goal) => {
+          addNotification({
+            type: 'info',
+            title: 'New goal scored!',
+            message: `Player: ${getPlayerName(goal.scorerPlayerId)}`,
+          });
+        });
+      }
+    },
+  });
   const teams = useQuery('liigaTeams', liigaService.getTeams);
+  const players = useQuery('liigaPlayers', () => liigaService.getPlayers());
 
   const getTeamName = (teamId) => {
     return teams.data[teamId].name;
   };
 
-  if (games.isLoading || teams.isLoading) {
+  const getPlayerName = (playerId) => {
+    const player = players.data.find((player) => player.id === playerId);
+    return `${player.lastName} ${player.firstName}`;
+  };
+
+  if (games.isLoading || teams.isLoading || players.isLoading) {
     return (
       <div className='w-full h-48 flex justify-center items-center'>
         <Spinner size='lg' />
@@ -21,7 +63,20 @@ export const LiveGames = () => {
   }
 
   return (
-    <ContentLayout darkBg={true} title='Live Games'>
+    <ContentLayout
+      darkBg={true}
+      title='Live Games'
+      buttonElement={
+        <button onClick={() => games.refetch()}>
+          <RefreshIcon
+            className={classNames(
+              games.isRefetching ? 'animate-spin-fast rotate-[179deg]' : '',
+              'w-6 h-6'
+            )}
+          />
+        </button>
+      }
+    >
       {games.data.length > 0 ? (
         <ul className='grid grid-cols-1 gap-6 mt-4'>
           {games.data.map((game) => (
