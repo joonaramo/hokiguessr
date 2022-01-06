5c01f82a7006ffb4831155b2599d796e40605cf8
joonaramo <joona00ramo@gmail.com>
2022-01-06 10:37:50 +0200

    feat(backend): cast tinyint values from db to booleans

--- server/src/utils/connectDB.js
+++ server/src/utils/connectDB.js
@@ -6,6 +6,13 @@ const connectionPool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  typeCast: function (field, next) {
+    if (field.type === 'TINY' && field.length === 1) {
+      return field.string() === '1'; // 1 = true, 0 = false
+    } else {
+      return next();
+    }
+  },
 });
 
 module.exports = connectionPool;
