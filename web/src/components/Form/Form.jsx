577e949e8632765a89008b258709a1467469c3d8
joonaramo <joona00ramo@gmail.com>
2022-01-11 12:37:46 +0200

    refactor(frontend): cleanup classnames on form component

--- web/src/components/Form/Form.jsx
+++ web/src/components/Form/Form.jsx
@@ -1,14 +1,17 @@
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
+import { classNames } from '../../utils/classnames';
 
 export const Form = ({ onSubmit, children, className, options, schema }) => {
   const methods = useForm({
     ...options,
     resolver: schema && yupResolver(schema),
   });
-  const classes = className + ' space-y-6';
   return (
-    <form className={classes} onSubmit={methods.handleSubmit(onSubmit)}>
+    <form
+      className={classNames(className, 'space-y-6')}
+      onSubmit={methods.handleSubmit(onSubmit)}
+    >
       {children(methods)}
     </form>
   );
