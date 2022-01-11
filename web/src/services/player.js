c2687b6c75060beab69d78f3f002f40479a49352
joonaramo <joona00ramo@gmail.com>
2022-01-11 20:52:05 +0200

    refactor(frontend): move admin related requests to their own service

--- web/src/services/player.js
+++ web/src/services/player.js
@@ -4,28 +4,13 @@ const getPlayers = async (page) => {
   return await axios.get('/players', { params: { page } });
 };
 
-const createPlayer = async (data) => {
-  return await axios.post('/players', data);
-};
-
 const getPlayer = async (id) => {
   return await axios.get(`/players/${id}`);
-};
-
-const updatePlayer = async (id, data) => {
-  return await axios.patch(`/players/${id}`, data);
-};
-
-const deletePlayer = async (id) => {
-  return await axios.delete(`/players/${id}`);
 };
 
 const playerService = {
   getPlayers,
   getPlayer,
-  createPlayer,
-  updatePlayer,
-  deletePlayer,
 };
 
 export default playerService;
